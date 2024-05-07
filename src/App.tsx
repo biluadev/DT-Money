1b52b16c43c72df5ca2fd413c241302dfe5720cf
biluadev <biluadevcod@gmail.com>
2024-05-07 20:21:41 +0100

    Alimentando a app com json.server

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyles } from "./styles/global";
 import { Transactions } from "./pages/Tansactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles/>
-     <Transactions>
-      
-     </Transactions>
-    </ThemeProvider>
+      <GlobalStyles />
+
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+    </ThemeProvider >
   )
 }
--- src/pages/Tansactions/index.tsx
+++ src/pages/Tansactions/index.tsx
@@ -1,39 +1,36 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext);
     return (
         <div>
             <Header />
             <Summary />
 
             <TransactionsContainer>
-                <SearchForm/>
+                <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td>Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-
-                        <tr>
-                            <td>Hambúrguer</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>13/04/2022</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr>
+                                    <tr key={transaction.id}></tr>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighlight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
